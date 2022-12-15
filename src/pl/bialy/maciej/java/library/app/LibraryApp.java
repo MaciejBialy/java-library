6649369e190567c48c2fe100d9f76ac01c5d4457
Maciej Biały <kmaciej865@gmail.com>
2022-12-15 17:16:01 +0100

    changed version app

--- src/pl/bialy/maciej/java/library/app/LibraryApp.java
+++ src/pl/bialy/maciej/java/library/app/LibraryApp.java
@@ -1,11 +1,12 @@
 package pl.bialy.maciej.java.library.app;
 
 public class LibraryApp {
-    private static final String APP_NAME = "Biblioteka v2.7";
+    private static final String APP_NAME = "Biblioteka v3.0";
 
     public static void main(String[] args) {
         System.out.println(APP_NAME);
         LibraryControl libControl = new LibraryControl();
         libControl.controlLoop();
+
     }
 }
